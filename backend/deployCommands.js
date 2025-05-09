8b85303b019c78c9d6599cf0f0fe6b5a63e7a570
maxsalzberg <maksims.kurdjukovs@gmail.com>
2025-05-10 01:57:27 +0200

    fix deploy

--- backend/deployCommands.js
+++ backend/deployCommands.js
@@ -1,41 +1,51 @@
+/*
+This script pushes all commands in the commands folder to be usable in discord.
+*/
+
 require('dotenv').config();
 const token = process.env.DISCORD_TOKEN;
 const clientID = process.env.DISCORD_CLIENT_ID;
 const guildID = process.env.DISCORD_GUILD_ID;
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
-const path = require('node:path');
 
 const commands = [];
 
-// Get all commands from the correct folder
-const commandsPath = path.join(__dirname, '../commands'); // исправлено
-const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+// Get all commands from the commands folder
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 console.log(commandFiles);
 
 for (const file of commandFiles) {
-    const command = require(path.join(commandsPath, file));
-    if ('data' in command && 'execute' in command) {
-        commands.push(command.data.toJSON());
-    } else {
-        console.log(`[WARNING] The command at ${file} is missing "data" or "execute".`);
-    }
+    const command = require(`../commands/${file}`);
+    commands.push(command.data.toJSON());
 }
 
 const rest = new REST({ version: '10' }).setToken(token);
 
+// console.log(commands);
+
 (async () => {
     try {
+        const rest = new REST({ version: '10' }).setToken(token);
+        
         console.log('Started refreshing application (/) commands.');
 
-        const data = await rest.put(
-            guildID
-                ? Routes.applicationGuildCommands(clientID, guildID)
-                : Routes.applicationCommands(clientID),
-            { body: commands },
-        );
+        //publish to guild if guildID is set, otherwise publish to global
+        if (guildID) {
+            const data = await rest.put(
+                Routes.applicationGuildCommands(clientID, guildID),
+                { body: commands },
+            );
+            console.log('Successfully reloaded '+ data.length +' commands.');
+        } else {
+            const data = await rest.put(
+                Routes.applicationCommands(clientID),
+                { body: commands },
+            );
+            console.log('Successfully reloaded '+ data.length +' commands.');
+        }
 
-        console.log(`Successfully reloaded ${data.length} commands.`);
     } catch (error) {
         console.error(error);
     }
