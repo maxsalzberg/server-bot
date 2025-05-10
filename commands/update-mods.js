e08be67b7f94c4ea6eccac41cfc676636f3a74c1
maxsalzberg <maksims.kurdjukovs@gmail.com>
2025-05-10 02:57:14 +0200

    fix

--- commands/update-mods.js
+++ commands/update-mods.js
@@ -1,6 +1,6 @@
 const { SlashCommandBuilder } = require('discord.js');
 const fs = require('fs');
-const restartCommand = require('./restart'); // Импортируем команду перезапуска
+const { Docker } = require('node-docker-api'); // Docker API for interacting with Docker via socket
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -28,16 +28,31 @@ module.exports = {
         // Write the updated content back to the file
         fs.writeFileSync(filePath, updatedYaml, 'utf8');
 
+        // Create a Docker client to interact with Docker via the socket
+        const docker = new Docker({ socketPath: '/var/run/docker.sock' });
+
         try {
             // Reply to the user confirming the mod list update
             await interaction.reply(`Mod list updated: ${mods}. The server is restarting...`);
 
-            // Call the restart container command (using the imported execute function)
-            await restartCommand.execute(interaction);
+            // Get the list of containers, filtered by the fixed container name
+            const containers = await docker.container.list({ all: true, filters: { name: ['north-server'] } });
+
+            // If the container doesn't exist, send a follow-up error message
+            if (containers.length === 0) {
+                await interaction.followUp('Container "north-server" does not exist.');
+                throw new Error('Container "north-server" does not exist.');
+            }
+
+            // Restart the container
+            await containers[0].restart();
+
+            // Confirm that the container was successfully restarted
+            await interaction.followUp('The server has been successfully restarted.');
         } catch (error) {
             // Handle error and inform the user
             console.error(error);
-            await interaction.followUp(`An error occurred while updating mods: ${error.message}`);
+            await interaction.followUp(`An error occurred while trying to restart the container: ${error.message}`);
         }
     },
 };
